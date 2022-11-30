0261ab7fd93a973471ab7d05477081bce37b7e77
Morsala <yuliya.neskazhu.1999@mail.ru>
2022-11-30 18:23:39 +0300

    Update app.js

--- 6 задание. Simple AJAX/app.js
+++ 6 задание. Simple AJAX/app.js
@@ -1,15 +1,18 @@
 console.log('hello')
+var btn_about = document.getElementById('btn_about')
+var btn_cont = document.getElementById('btn_cont')
+var cont = document.getElementById('cont')
 
 document.body.onload = () => {
     console.log('hello onload')
     let request = new XMLHttpRequest;
-    request.open('GET', './header.html', true)
+    request.open('GET', 'about.html', true)
     request.send()
     request.onreadystatechange = function () {
         if (request.readyState === 4)
         if (request.status === 200) {
             console.log(request.responseText)
-            document.body.innerHTML = request.responseText + document.body.innerHTML
+            cont.innerHTML = request.responseText
         }
     }
 }
